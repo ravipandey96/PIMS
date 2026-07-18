140138447d99b1e4f0e303d0d29b76d4f8d5095c
Ravi Pandey <ravishankarpandey96@gmail.com>
2026-07-18 18:52:10 +0530

    Configure Persistence dependency injection

--- PIMS.Persistence/DependencyInjection.cs
+++ PIMS.Persistence/DependencyInjection.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PIMS.Persistence.Context;
 
-namespace PIMS.Persistence
+namespace PIMS.Persistence;
+
+/// <summary>
+/// Registers Persistence layer services.
+/// </summary>
+public static class DependencyInjection
 {
-    internal class DependencyInjection
+    /// <summary>
+    /// Registers the Persistence layer.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <param name="configuration">Application configuration.</param>
+    /// <returns>Service collection.</returns>
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<PimsDbContext>(options =>
+        {
+            options.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString));
+        });
+
+        return services;
     }
 }
