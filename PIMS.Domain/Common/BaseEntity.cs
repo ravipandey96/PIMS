9b98c08ed341a702c1d7592ac296a7d0f89a165e
Ravi Pandey <ravishankarpandey96@gmail.com>
2026-07-18 17:16:27 +0530

    Add BaseEntity

--- PIMS.Domain/Common/BaseEntity.cs
+++ PIMS.Domain/Common/BaseEntity.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace PIMS.Domain.Common;
 
-namespace PIMS.Domain.Common
+/// <summary>
+/// Represents the base entity for all domain entities.
+/// Provides the primary key shared by all entities.
+/// </summary>
+public abstract class BaseEntity
 {
-    internal class BaseEntity
-    {
-    }
+    /// <summary>
+    /// Gets or sets the unique identifier for the entity.
+    /// </summary>
+    public int Id { get; set; }
 }
