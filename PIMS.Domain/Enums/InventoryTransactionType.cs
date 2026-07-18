4184be4cc15752228356bdd5220d7d76a8c9c7c2
Ravi Pandey <ravishankarpandey96@gmail.com>
2026-07-18 17:30:17 +0530

    Add domain enums for user roles and inventory transaction types

--- PIMS.Domain/Enums/InventoryTransactionType.cs
+++ PIMS.Domain/Enums/InventoryTransactionType.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace PIMS.Domain.Enums;
 
-namespace PIMS.Domain.Enums
+/// <summary>
+/// Represents the different types of inventory transactions.
+/// </summary>
+public enum InventoryTransactionType
 {
-    internal class InventoryTransactionType
-    {
-    }
+    /// <summary>
+    /// Stock added to inventory.
+    /// </summary>
+    StockIn = 1,
+
+    /// <summary>
+    /// Stock removed from inventory.
+    /// </summary>
+    StockOut = 2,
+
+    /// <summary>
+    /// Manual inventory adjustment.
+    /// </summary>
+    Adjustment = 3
 }
--- PIMS.Domain/Enums/UserRole.cs
+++ PIMS.Domain/Enums/UserRole.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace PIMS.Domain.Enums;
 
-namespace PIMS.Domain.Enums
+/// <summary>
+/// Defines the roles available in the Product Inventory Management System.
+/// </summary>
+public enum UserRole
 {
-    internal class UserRole
-    {
-    }
+    /// <summary>
+    /// System Administrator with full access.
+    /// </summary>
+    Administrator = 1,
+
+    /// <summary>
+    /// Inventory Manager responsible for stock management.
+    /// </summary>
+    InventoryManager = 2,
+
+    /// <summary>
+    /// Standard employee with limited access.
+    /// </summary>
+    Employee = 3
 }
