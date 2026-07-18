6505f38063f9dbbaf021dd8f956141eea43ebce0
Ravi Pandey <ravishankarpandey96@gmail.com>
2026-07-18 18:44:50 +0530

    Add EF Core Inventory entity configuration

--- PIMS.Persistence/Configurations/InventoryConfiguration.cs
+++ PIMS.Persistence/Configurations/InventoryConfiguration.cs
@@ -1,10 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PIMS.Domain.Entities;
 
-namespace PIMS.Persistence.Configurations
+namespace PIMS.Persistence.Configurations;
+
+/// <summary>
+/// Configures the Inventory entity.
+/// </summary>
+public class InventoryConfiguration : IEntityTypeConfiguration<Inventory>
 {
-    internal class InventoryConfiguration
+    /// <summary>
+    /// Configures the Inventory entity.
+    /// </summary>
+    /// <param name="builder">Entity type builder.</param>
+    public void Configure(EntityTypeBuilder<Inventory> builder)
     {
+        // Table Name
+        builder.ToTable("Inventories");
+
+        // Primary Key
+        builder.HasKey(i => i.Id);
+
+        // Id
+        builder.Property(i => i.Id)
+               .ValueGeneratedOnAdd();
+
+        // ProductId
+        builder.Property(i => i.ProductId)
+               .IsRequired();
+
+        // Quantity
+        builder.Property(i => i.Quantity)
+               .IsRequired();
+
+        // Low Stock Threshold
+        builder.Property(i => i.LowStockThreshold)
+               .IsRequired();
+
+        // Warehouse Location
+        builder.Property(i => i.WarehouseLocation)
+               .HasMaxLength(200);
+
+        // Audit Fields
+        builder.Property(i => i.CreatedOn)
+               .IsRequired();
+
+        builder.Property(i => i.CreatedBy);
+
+        builder.Property(i => i.ModifiedOn);
+
+        builder.Property(i => i.ModifiedBy);
+
+        // One Product -> One Inventory
+        builder.HasOne(i => i.Product)
+               .WithOne(p => p.Inventory)
+               .HasForeignKey<Inventory>(i => i.ProductId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        // One Inventory -> Many Inventory Transactions
+        builder.HasMany(i => i.InventoryTransactions)
+               .WithOne(it => it.Inventory)
+               .HasForeignKey(it => it.InventoryId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        // ProductId must be unique (One-to-One)
+        builder.HasIndex(i => i.ProductId)
+               .IsUnique();
     }
 }
