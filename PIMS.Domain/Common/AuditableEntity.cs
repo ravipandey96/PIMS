38453c7f8ce927f1244930be6e3551bd0bf7a57f
Ravi Pandey <ravishankarpandey96@gmail.com>
2026-07-18 17:27:05 +0530

    Add AuditableEntity base class

--- PIMS.Domain/Common/AuditableEntity.cs
+++ PIMS.Domain/Common/AuditableEntity.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace PIMS.Domain.Common;
 
-namespace PIMS.Domain.Common
+/// <summary>
+/// Represents a base entity that includes audit information.
+/// All auditable entities should inherit from this class.
+/// </summary>
+public abstract class AuditableEntity : BaseEntity
 {
-    internal class AuditableEntity
-    {
-    }
+    /// <summary>
+    /// Gets or sets the UTC date and time when the entity was created.
+    /// </summary>
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    /// <summary>
+    /// Gets or sets the identifier of the user who created the entity.
+    /// </summary>
+    public int? CreatedBy { get; set; }
+
+    /// <summary>
+    /// Gets or sets the UTC date and time when the entity was last modified.
+    /// </summary>
+    public DateTime? ModifiedOn { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the user who last modified the entity.
+    /// </summary>
+    public int? ModifiedBy { get; set; }
 }
